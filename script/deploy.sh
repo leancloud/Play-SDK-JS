fd528ec34c8b2924abe288e7d43e8af4f62ca617
onerain <onerain88@gmail.com>
2018-07-04 13:26:03 +0800

    test: ci

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,6 +7,6 @@ git config user.name "leancloud-bot";
 git config user.email "ci@leancloud.cn";
 git add .;
 git commit -m "Deploy demos to Github Pages [skip ci]";
-git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages;
+git push -qf https://github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages;
 echo "done.";
 cd ..
