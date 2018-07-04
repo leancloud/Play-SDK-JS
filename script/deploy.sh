0ce412a786c66878a4fd35fd737ec0bc8a8a75f0
onerain <onerain88@gmail.com>
2018-07-04 12:16:59 +0800

    test: ci

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,6 +7,6 @@ git config user.name "onerain88";
 git config user.email "onerain88@gmail.com";
 git add .;
 git commit -m "Deploy demos to Github Pages [skip ci]";
-git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git doc:gh-pages;
+git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages;
 echo "done.";
 cd ..
