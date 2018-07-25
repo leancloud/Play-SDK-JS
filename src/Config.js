17c43483da6441dc05fcec6087ad4606ef9b8154
onerain <onerain88@gmail.com>
2018-07-25 11:49:15 +0800

    refactor: 调整顶级域名配置；更换 axios 为 superagent，适配小程序发布

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,7 @@
 // SDK 版本号
 export { version as PlayVersion } from '../package.json';
-export const NorthCNServerURL = 'https://game-router-cn-n1.leancloud.cn/';
-export const EastCNServerURL = 'https://game-router-cn-e1.leancloud.cn/';
-export const USServerURL = 'https://game-router-us-w1.leancloud.cn/';
+export const NorthCNServerURL =
+  'https://game-router-cn-n1.leancloud.cn/v1/router';
+export const EastCNServerURL =
+  'https://game-router-cn-e1.leancloud.cn/v1/router';
+export const USServerURL = 'https://game-router-us-w1.leancloud.cn/v1/router';
