43b3370e2ed5d07c7dfb0cecc162eecdc7f26427
onerain <onerain88@gmail.com>
2018-08-16 09:52:03 +0800

    test: 增加加入大厅之后，创建 / 加入房间的单元测试

--- test/Lobby.test.js
+++ test/Lobby.test.js
@@ -91,4 +91,33 @@ describe('test lobby', () => {
     play2.connect();
     play3.connect();
   });
+
+  it('test autoJoinLobby', done => {
+    const play = new Play();
+    play.autoJoinLobby = true;
+    play.init({
+      appId: APP_ID,
+      appKey: APP_KEY,
+      region: Region.EastChina,
+    });
+    play.userId = 'play';
+    play.on(Event.LOBBY_JOINED, () => {
+      play.joinOrCreateRoom('lt3_room');
+    });
+    play.on(Event.ROOM_CREATED, () => {
+      debug('room created');
+    });
+    play.on(Event.ROOM_CREATE_FAILED, () => {
+      debug('room create failed');
+    });
+    play.on(Event.ROOM_JOINED, () => {
+      debug('room joined');
+      done();
+    });
+    play.on(Event.ROOM_JOIN_FAILED, err => {
+      debug(`room join failed: ${err}`);
+    });
+
+    play.connect();
+  });
 });
