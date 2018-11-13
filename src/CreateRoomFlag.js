c721f29d6f93df666e3f30e9f565938de7e7f58b
onerain <onerain88@gmail.com>
2018-11-13 18:42:03 +0800

    remove: 移除 CreateRoomFlag.MasterSetMaster 标志，默认只允许 Master 设置 Master

--- src/CreateRoomFlag.js
+++ src/CreateRoomFlag.js
@@ -12,10 +12,6 @@ const CreateRoomFlag = {
    * 只允许 Master 设置房间属性
    */
   MasterUpdateRoomProperties: 2,
-  /**
-   * 只允许 Master 设置 Master
-   */
-  MasterSetMaster: 4,
 };
 
 export default CreateRoomFlag;
