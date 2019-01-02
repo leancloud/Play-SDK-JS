db43f78733a946892f45bcbb7104c120bd3c9de1
oneRain <onerain88@gmail.com>
2019-01-02 10:45:19 +0800

    chore: 优化 promise 用法

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -9,43 +9,42 @@ export default class AppRouter {
   }
 
   fetch() {
-    return new Promise((resolve, reject) => {
-      const now = Date.now();
-      if (now < this._serverValidTimestamp) {
-        // 在有效期内，则直接返回缓存数据
-        debug(`get app router from cache: ${this._url}`);
-        resolve(this._url);
-      } else {
-        this._fetch(resolve, reject);
-      }
-    });
+    const now = Date.now();
+    if (now < this._serverValidTimestamp) {
+      // 在有效期内，则直接返回缓存数据
+      debug(`get app router from cache: ${this._url}`);
+      return Promise.resolve(this._url);
+    }
+    return this._fetch();
   }
 
-  _fetch(resolve, reject) {
-    try {
-      const url = 'https://app-router.leancloud.cn/2/route';
-      const payload = {
-        appId: this._appId,
-      };
-      this._httpReq = request
-        .get(url)
-        .query(payload)
-        .end((err, response) => {
-          if (err) {
-            reject(err);
-          } else {
-            const body = JSON.parse(response.text);
-            const { ttl, play_server: playServer } = body;
-            this._url = `https://${playServer}/1/multiplayer/router/router`;
-            this._serverValidTimestamp = Date.now() + ttl * 1000;
-            debug(`server valid timestamp: ${this._serverValidTimestamp}`);
-            debug(`get app router from server: ${this._url}`);
-            resolve(this._url);
-          }
-        });
-    } catch (err) {
-      reject(err);
-    }
+  _fetch() {
+    return new Promise((resolve, reject) => {
+      try {
+        const url = 'https://app-router.leancloud.cn/2/route';
+        const payload = {
+          appId: this._appId,
+        };
+        this._httpReq = request
+          .get(url)
+          .query(payload)
+          .end((err, response) => {
+            if (err) {
+              reject(err);
+            } else {
+              const body = JSON.parse(response.text);
+              const { ttl, play_server: playServer } = body;
+              this._url = `https://${playServer}/1/multiplayer/router/router`;
+              this._serverValidTimestamp = Date.now() + ttl * 1000;
+              debug(`server valid timestamp: ${this._serverValidTimestamp}`);
+              debug(`get app router from server: ${this._url}`);
+              resolve(this._url);
+            }
+          });
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
 
   abort() {
