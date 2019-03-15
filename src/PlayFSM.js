4bdbeeea996b5ebeb737d6678ca630a9183c43b1
oneRain <onerain88@gmail.com>
2019-03-15 17:58:29 +0800

    chore: 移除 websocket 的关闭的 promise 等待

--- src/PlayFSM.js
+++ src/PlayFSM.js
@@ -103,7 +103,7 @@ const PlayFSM = machina.Fsm.extend({
       _onEnter() {
         debug('lobby _onEnter()');
         this._lobbyConn.on(ERROR_EVENT, async ({ code, detail }) => {
-          await this._lobbyConn.close();
+          this._lobbyConn.close();
           this._play.emit(Event.ERROR, {
             code,
             detail,
@@ -229,7 +229,7 @@ const PlayFSM = machina.Fsm.extend({
         this._play._lastRoomId = this._play.room.name;
         // 注册事件
         this._gameConn.on(ERROR_EVENT, async ({ code, detail }) => {
-          await this._gameConn.close();
+          this._gameConn.close();
           this._play.emit(Event.ERROR, {
             code,
             detail,
@@ -314,7 +314,7 @@ const PlayFSM = machina.Fsm.extend({
         });
         this._gameConn.on(ROOM_KICKED_EVENT, async (code, msg) => {
           this.handle('onTransition', 'gameToLobby');
-          await this._gameConn.close();
+          this._gameConn.close();
           await this._connectLobby();
           this.handle('onTransition', 'lobby');
           this._play.emit(Event.ROOM_KICKED, { code, msg });
@@ -342,7 +342,7 @@ const PlayFSM = machina.Fsm.extend({
         return new Promise(async (resolve, reject) => {
           try {
             await this._gameConn.leaveRoom();
-            await this._gameConn.close();
+            this._gameConn.close();
             await this._connectLobby().then(
               tap(() => {
                 this.handle('onTransition', 'lobby');
@@ -675,7 +675,7 @@ const PlayFSM = machina.Fsm.extend({
         await this._lobbyConn.openSession(appId, userId, gameVersion);
         resolve(this._play);
       } catch (err) {
-        await this._lobbyConn.close();
+        this._lobbyConn.close();
         reject(err);
       }
     });
@@ -700,7 +700,7 @@ const PlayFSM = machina.Fsm.extend({
         await this._gameConn.openSession(appId, userId, gameVersion);
         resolve();
       } catch (err) {
-        await this._gameConn.close();
+        this._gameConn.close();
         reject(err);
       }
     });
@@ -715,10 +715,10 @@ const PlayFSM = machina.Fsm.extend({
           expectedUserIds
         );
         this._initGame(gameRoom);
-        await this._lobbyConn.close();
+        this._lobbyConn.close();
         resolve();
       } catch (err) {
-        await this._gameConn.close();
+        this._gameConn.close();
         reject(err);
       }
     });
@@ -733,10 +733,10 @@ const PlayFSM = machina.Fsm.extend({
           expectedUserIds
         );
         this._initGame(gameRoom);
-        await this._lobbyConn.close();
+        this._lobbyConn.close();
         resolve();
       } catch (err) {
-        await this._gameConn.close();
+        this._gameConn.close();
         reject(err);
       }
     });
