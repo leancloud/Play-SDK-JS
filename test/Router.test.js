6d09ef9fa5d78a073a1e2bfbca6a0a82ddbb3a80
oneRain <onerain88@gmail.com>
2019-06-25 16:51:02 +0800

    chore: 调整用户自定义域名配置方式（只填写域名） (#218)
    * chore: 调整用户自定义域名配置方式（只填写域名）
    
    * chore:
    
    * chore

--- test/Router.test.js
+++ test/Router.test.js
@@ -13,7 +13,7 @@ describe('test router', () => {
       appId: 'FQr8l8LLvdxIwhMHN77sNluX-9Nh9j0Va',
       appKey: 'MJSm46Uu6LjF5eNmqfbuUmt6',
       userId: 'tar1',
-      playServer: 'https://game-router-cn-e1.leancloud.cn/v1',
+      playServer: 'https://fqr8l8ll.play.lncldapi.com',
     });
     await p.connect();
     await p.createRoom();
