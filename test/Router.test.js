0527a573e2230a61b257328a2824214c3f5a2427
oneRain <onerain88@gmail.com>
2020-03-31 17:40:17 +0800

    chore

--- test/Router.test.js
+++ test/Router.test.js
@@ -36,20 +36,4 @@ describe('test router', () => {
     await p.connect();
     await p.close();
   });
-
-  // it('test fallback router', done => {
-  //   debug(getFallbackRouter('FQr8l8LLvdxIwhMHN77sNluX-9Nh9j0Va'));
-  //   expect(getFallbackRouter('FQr8l8LLvdxIwhMHN77sNluX-9Nh9j0Va')).to.be.equal(
-  //     'https://fqr8l8ll.play.lncldapi.com/1/multiplayer/router/route'
-  //   );
-  //   debug(getFallbackRouter('FQr8l8LLvdxIwhMHN77sNluX-MdYXbMMI'));
-  //   expect(getFallbackRouter('FQr8l8LLvdxIwhMHN77sNluX-MdYXbMMI')).to.be.equal(
-  //     'https://fqr8l8ll.play.lncldglobal.com/1/multiplayer/router/route'
-  //   );
-  //   debug(getFallbackRouter('BMYV4RKSTwo8WSqt8q9ezcWF-gzGzoHsz'));
-  //   expect(getFallbackRouter('BMYV4RKSTwo8WSqt8q9ezcWF-gzGzoHsz')).to.be.equal(
-  //     'https://bmyv4rks.play.lncld.com/1/multiplayer/router/route'
-  //   );
-  //   done();
-  // });
 });
