9971ebfebdd5c0850e14b1ba6d43999ef8aee8cc
sdjdd <i@sdjdd.com>
2020-12-01 12:53:20 +0800

    fix: add missing event types

--- play.d.ts
+++ play.d.ts
@@ -15,12 +15,26 @@ declare class EventEmitter<T> {
 }
 
 export enum Event {
+  /** 连接成功 */
+  CONNECTED = 'connected',
+  /** 连接失败 */
+  CONNECT_FAILED = 'connectFailed',
   /** 断开连接 */
   DISCONNECTED = 'disconnected',
   /** 加入到大厅 */
   LOBBY_JOINED = 'lobbyJoined',
+  /** 离开大厅 */
+  LOBBY_LEFT = 'lobbyLeft',
   /** 大厅房间列表变化 */
   LOBBY_ROOM_LIST_UPDATED = 'lobbyRoomListUpdate',
+  /** 创建房间成功 */
+  ROOM_CREATED = 'roomCreated',
+  /** 房间创建失败 */
+  ROOM_CREATE_FAILED = 'roomCreateFailed',
+  /** 加入房间成功 */
+  ROOM_JOINED = 'roomJoined',
+  /** 加入房间失败 */
+  ROOM_JOIN_FAILED = 'roomJoinFailed',
   /** 有新玩家加入房间 */
   PLAYER_ROOM_JOINED = 'newPlayerJoinedRoom',
   /** 有玩家离开房间 */
@@ -35,6 +49,10 @@ export enum Event {
   ROOM_KICKED = 'roomKicked',
   /** 房间系统属性变化 */
   ROOM_SYSTEM_PROPERTIES_CHANGED = 'roomSystemPropertiesChanged',
+  /** 房间「开启 / 关闭」 */
+  ROOM_OPEN_CHANGED = 'roomOpenChanged',
+  /** 房间「可见 / 不可见」 */
+  ROOM_VISIBLE_CHANGED = 'roomVisibleChanged',
   /** 房间自定义属性变化 */
   ROOM_CUSTOM_PROPERTIES_CHANGED = 'roomCustomPropertiesChanged',
   /** 玩家自定义属性变化 */
