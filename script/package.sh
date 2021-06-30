bbf580e33235625f4849eb88fc1a7e1ddea67894
oneRain <onerain88@gmail.com>
2021-06-30 14:41:52 +0800

    feat: ci/cd action (#271)

--- script/package.sh
+++ script/package.sh
@@ -3,3 +3,10 @@ mkdir play
 cp ./dist/play.js ./dist/play.min.js ./play.d.ts ./play
 zip -r play-egret.zip play
 rm -r play
+
+mkdir release
+cp play-egret.zip ./release
+cp ./dist/play-laya.js ./release
+cp ./dist/play-weapp.js ./release
+cp ./dist/play.js ./release
+cp play.d.ts ./release
